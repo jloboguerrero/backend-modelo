facfb386a838e803e097f663e383be7a06129a24
Jonathan Loboguerrero <joboguerrero@gmail.com>
2021-01-15 06:33:03 -0500

    Fin seccion 10 - Backend

---
+++ routes/mensajes.js
@@ -0,0 +1,16 @@
+/*
+    Path: /apu/mensajes
+*/
+
+const { Router } = require('express');
+const { obtenerChat } = require('../controllers/mensajes');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+
+
+router.get('/:de', validarJWT, obtenerChat);
+
+
+module.exports = router;
--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,14 +1,43 @@
 // Mensajes de sockets
 
+const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
+const { usuarioConectado, usuarioDesconectado, grabarMensaje } = require('../controllers/socket');
 
 // Mensajes de Sockets
 io.on('connection', client => {
 
     console.log('Cliente conectado al Backend');
 
+    //console.log(client.handshake.headers['x-token']);
+    const [ valido, uid ] = comprobarJWT(client.handshake.headers['x-token']);
+
+    //console.log(valido, uid);
+
+    // Verificar Auteniticación
+    if(!valido){ return client.disconnect(); }
+    console.log('Cliente autenticado');
+
+    // Cliente autenticado
+    usuarioConectado( uid );
+
+    // Ingresar al usuario a una sala en particular
+    // sala global, client.id, 
+    client.join( uid );
+
+    // Escuchar del cliente el mensaje-personal
+    client.on('mensaje-personal', async ( payload ) => {
+        console.log(payload);
+        // TODO: Grabar mensaje
+        await grabarMensaje(payload);
+
+        io.to( payload.para ).emit('mensaje-personal', payload);
+    })
+
+
     client.on('disconnect', () => {
         console.log('Cliente desconectado del Backend');
+        usuarioDesconectado( uid );
     });
 
     client.on('mensaje', ( payload ) => {
